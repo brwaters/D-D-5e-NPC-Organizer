de995afbe05dd05b321e1529667869c1ecfed479
Brendon Waters <brendon.waters117@gmail.com>
2019-03-20 17:36:24 -0700

    Finally managed to get scrollView working properly. TextView constraints still buggered.

--- D&D 5e NPC Organizer/ViewController.swift
+++ D&D 5e NPC Organizer/ViewController.swift
@@ -13,8 +13,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
+    @IBAction func cancelButton(_ sender: Any) {
+        if presentingViewController is UINavigationController {
+            // Add
+            dismiss(animated: true, completion: nil)
+        }
+        else if let owningNavigationController = navigationController { // Edit
+            owningNavigationController.popViewController(animated: true)
+        }
 
+    }
+    
 }
 
