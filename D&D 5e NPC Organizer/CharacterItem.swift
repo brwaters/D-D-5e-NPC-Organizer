611a6403fc503e537655ca788605a0e484609854
Brendon Waters <brendon.waters117@gmail.com>
2019-03-22 19:20:10 -0700

    EditView shows a default character now. Need to change so that only selects default character if segue from 'Add'. Edit should display previous. Also need to implement Save.

--- D&D 5e NPC Organizer/CharacterItem.swift
+++ D&D 5e NPC Organizer/CharacterItem.swift
@@ -61,10 +61,10 @@ class CharacterItem: NSObject/*, NSCoding*/ {
             self.image = image
             self.name = "Strahd Von Zarovich"
             self.characterDescription = "Medium undead"
-            self.alignment = "lawful evil"
+            self.alignment = "Lawful evil"
             
             self.armorClass = "16 (Natural Armor)"
-            self.hitPoints = "144 (17d8 + 68"
+            self.hitPoints = "144 (17d8 + 68)"
             self.speed = "30 ft."
             self.abilityScore = ["STR" : "18 (+4)", "DEX" : "18 (+4)", "CON" : "18 (+4)", "INT" : "20 (+5)", "WIS" : "15 (+2)", "CHA" : "18 (+4)"]
             self.savingThrows = "DEX +9, WIS +7, CHA +9"
@@ -77,7 +77,7 @@ class CharacterItem: NSObject/*, NSCoding*/ {
             
             self.abilities = ["Shapechanger" : "If Strahd isn’t in running water or sunlight, he can use his action to polymorph into a Tiny bat, a Medium wolf, or a Medium cloud of mist, or back into his true form. While in bat or wolf form, Strahd can’t speak. In bat form, his walking speed is 5 feet, and he has a flying speed of 30 feet. In wolf form, his walking speed is 40 feet. His statistics, other than his size and speed, are unchanged. Anything he is wearing transforms with him, but nothing he is carrying does. He reverts to his true form if he dies. While in mist form, Strahd can’t take any actions, speak, or manipulate objects. He is weightless, has a flying speed of 20 feet, can hover, and can enter a hostile creature’s space and stop there. In addition, if air can pass through a space, the mist can do so without squeezing, and he can’t pass through water. He has advantage on Strength, Dexterity, and Constitution saving throws, and he is immune to all nonmagical damage, except the damage he takes from sunlight."]
             self.actions = ["Multiattack (Vampire Form Only" : "Strahd makes two attacks, only one of which can be a bite attack."]
-            self.legendaryActions = ["legendaryDecription" : "Strahd can take 3 legendary actions, choosing from the options below. Only one legendary action option can be used at a time and only at the end of another creature’s turn. Strahd regains spent legendary actions at the start of his turn.", "Move" : "Strahd moves up to his speed without provoking opportunity attacks."]
+            self.legendaryActions = ["legendaryDescription" : "Strahd can take 3 legendary actions, choosing from the options below. Only one legendary action option can be used at a time and only at the end of another creature’s turn. Strahd regains spent legendary actions at the start of his turn.", "Move" : "Strahd moves up to his speed without provoking opportunity attacks."]
             self.lairActions = ["lairDescriptions" : "While Strahd is in Castle Ravenloft, he can take lair actions as long as he isn’t incapacitated. On initiative count 20 (losing initiative ties), Strahd can take one of the following lair action options, or forgo using any of them in that round:", "actionOne" : "Until initiative count 20 of the next round, Strahd can pass through solid walls, doors, ceilings, and floors as if they weren’t there. Strahd targets any number of doors and windows that he can see, causing each one to either open or close as he wishes. Closed doors can be magically locked (needing a successful DC 20 Strength check to force open) until Strahd chooses to end the effect, or until Strahd uses this lair action again.", "actionTwo" : "Strahd summons the angry spirit of one who has died in the castle. The apparition appears next to a hostile creature that Strahd can see, makes an attack against that creature, and then disappears. The apparition has the statistics of a specter."]
     }
     
--- D&D 5e NPC Organizer/ViewController.swift
+++ D&D 5e NPC Organizer/ViewController.swift
@@ -18,6 +18,33 @@ class ViewController: UIViewController {
         nameText.text = defaultCharacter.name
         characteristicsText.text = defaultCharacter.characterDescription
         alignmentText.text = defaultCharacter.alignment
+        armorClassText.text = defaultCharacter.armorClass
+        hitPointsText.text  = defaultCharacter.hitPoints
+        speedText.text = defaultCharacter.speed
+        challengeRatingText.text = defaultCharacter.challengeRating
+        languagesText.text = defaultCharacter.languages
+        savingThrowsText.text = defaultCharacter.savingThrows
+        skillsText.text = defaultCharacter.skills
+        damageResistanceText.text = defaultCharacter.damageResistance
+        damageVulnerabilitiesText.text = defaultCharacter.damageVulnerability
+        sensesText.text = defaultCharacter.senses
+        miscellaneousText.text = "Miscellaneous"
+        
+        strengthText.text = defaultCharacter.abilityScore["STR"]
+        dexterityText.text = defaultCharacter.abilityScore["DEX"]
+        constitutionText.text = defaultCharacter.abilityScore["CON"]
+        intellectText.text = defaultCharacter.abilityScore["INT"]
+        wisdomText.text = defaultCharacter.abilityScore["WIS"]
+        charismaText.text = defaultCharacter.abilityScore["CHA"]
+        
+        abilitiesText.text = defaultCharacter.abilities["Shapechanger"]
+        
+        actionsText.text = defaultCharacter.actions["Multiattack (Vampire Form Only"]
+        
+        legendaryActionsText.text = defaultCharacter.actions["legendaryDescription"]
+        
+        lairActionsText.text = defaultCharacter.actions["lairDescription"]
+        
     }
 
     @IBAction func cancelButton(_ sender: Any) {
@@ -41,24 +68,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var hitPointsText: UITextField!
     @IBOutlet weak var speedText: UITextField!
     @IBOutlet weak var challengeRatingText: UITextField!
-    @IBOutlet weak var langaugesText: UITextField!
-    @IBOutlet weak var savingThrows: UITextField!
+    @IBOutlet weak var languagesText: UITextField!
+    @IBOutlet weak var savingThrowsText: UITextField!
     @IBOutlet weak var skillsText: UITextField!
     @IBOutlet weak var damageResistanceText: UITextField!
-    @IBOutlet weak var damageImmunitiesText: UITextField!
+    @IBOutlet weak var damageVulnerabilitiesText: UITextField!
     @IBOutlet weak var sensesText: UITextField!
     @IBOutlet weak var miscellaneousText: UITextField!
     
     @IBOutlet weak var strengthText: UITextView!
     @IBOutlet weak var dexterityText: UITextView!
     @IBOutlet weak var constitutionText: UITextView!
-    @IBOutlet var intellectText: [UITextView]!
+    @IBOutlet weak var intellectText: UITextView!
     @IBOutlet weak var wisdomText: UITextView!
     @IBOutlet weak var charismaText: UITextView!
     
     @IBOutlet weak var abilitiesText: UITextView!
     @IBOutlet weak var actionsText: UITextView!
     @IBOutlet weak var legendaryActionsText: UITextView!
+    @IBOutlet weak var lairActionsText: UITextView!
+    
+    
     
     
 }
